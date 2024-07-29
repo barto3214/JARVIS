b9c353e34bb402107ff386ab49f64635690f872e
Bartek Locksmith <bartek.slusarz@onet.pl>
2024-07-29 22:32:37 +0200

    Dodano wyszukiwanie webowe(WRESZCIE)

--- JARVIS 1.0.py
+++ JARVIS 1.0.py
@@ -1,24 +1,39 @@
 import pyttsx3 as speech
 import speech_recognition as recognition
-import datetime as time
+import datetime as time                                     #Wczytanie modułów
 import subprocess
 import pyautogui as fileoperational
 import psutil as systeminfo
 import GPUtil as GPUinfo
+import urllib.parse
 
 engine = speech.init()
 end = True
 recognizer = recognition.Recognizer()
 
 def speak(text):
-    engine.say(text)
+    engine.say(text)                            #Protokół odpowiadający za mowę
     engine.runAndWait()
+    
+def searcher(command):
+    if "wyszukaj" in command:
+        searchfraza = command.replace("wyszukaj", "").strip()
+        urlcoded = urllib.parse.quote(searchfraza)
+        search = f"https://www.google.com/search?q={urlcoded}"
+        
+        try:                                                                            # Wyszukiwanie w sieci
+            subprocess.Popen([chrome_path, search])
+            speak(f"Wyszukuję frazę {searchfraza} w przeglądarce")
+        except FileNotFoundError:
+            speak("Nie można odnaleźć przeglądarki Chrome. Sprawdź ścieżkę do aplikacji.")
+        return True
+    return False
 
-def recognize_speech(prompt="Czekam na komendy", language="pl-PL"):
+def recognize_speech(prompt="Słucham...", language="pl-PL"):
     while True:
         with recognition.Microphone() as mikro:
-            speak(prompt)
-            print(prompt)
+            speak("")
+            print(prompt)                                                               #Protokół rozpoznawania mowy
             audiodata = recognizer.listen(mikro)
         try:
             speech_text = recognizer.recognize_google(audiodata, language=language)
@@ -30,32 +45,41 @@ def recognize_speech(prompt="Czekam na komendy", language="pl-PL"):
             speak(f"Błąd: {error}")
 
 voices = engine.getProperty('voices')
+
 for voice in voices:
     if 'Microsoft Paulina Desktop' in voice.name:
-        engine.setProperty('voice', voice.id)
+        engine.setProperty('voice', voice.id)                                               #Dobranie głosu
         break
 
 speak("Bartek, jestem gotowy do pracy")
 
+chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"              #Ścieżka do Chrome
+
 while end:
-    speech_text = recognize_speech("Słucham...")
+    speech_text = recognize_speech()
     if "jarvis" in speech_text.lower():                                             
         command = speech_text.lower().replace("jarvis", "").strip()
         
         print("Przetworzona komenda: ", command)
+
         match command:
             case "przywitaj się":
                 speak("Siemanko")
+                
             case "podaj godzinę" | "która godzina" | "która jest godzina":
                 now = time.datetime.now().strftime("%H:%M")
                 speak(f"Obecnie jest {now}")
+                
             case "ustawienia mowy":
                 speak("Poproszę kod admina")
-                code = recognize_speech("Czekam na kod...")
+                code = recognize_speech("Czekam na kod...")                                             #Prostsze funkcjonalności
+                
                 if code == "1285":
                     speak("Jesteś w ustawieniach")
+                    
                 else:
                     speak("Rikojkoko kij ci w oko")
+                    
             case "otwórz notatnik":
                 speak("Otwieram Notatnik")
                 subprocess.Popen(['notepad'])
@@ -64,13 +88,17 @@ while end:
                     action = recognize_speech("Co chcesz z nim zrobić")
                     match action.lower():
                         case "napisać coś":
+                            
                             texta = True
                             while texta:
                                 texttowrite = recognize_speech("Co chcesz zapisać")
                                 fileoperational.write(texttowrite)
-                                if "przestań pisać" in texttowrite.lower():
+                                
+                                if "przestań pisać" in texttowrite.lower():                             #Notatnik
                                     texta = False
+                                    
                         case "zamknąć i zapisać":
+                            
                             fileoperational.hotkey('ctrl', 's')
                             textoend = recognize_speech("Jak nazwać plik?")
                             fileoperational.write(textoend)
@@ -78,11 +106,15 @@ while end:
                             fileoperational.hotkey('alt', 'f4')
                             nota = False
                             speak("Notatnik został zapisany i zamknięty")
+                            
             case "statystyki procesora":
+                
                 cpupercent = systeminfo.cpu_percent(interval=1)
                 speak(f"Użycie procesora w procentach wynosi {cpupercent}%")
+                
                 print(f"Użycie procesora w procentach wynosi {cpupercent}%")
             case "statystyki karty graficznej":
+                
                 gpustats = GPUinfo.getGPUs()
                 for gpu in gpustats:
                     print(f"Użycie karty graficznej: {gpu.load * 100}%")
@@ -96,16 +128,18 @@ while end:
                     speak(f"Temperatura karty graficznej to {gpu.temperature}°C")
                     
             case "statystyki pamięci ram":
+                
                 memoryinfo = systeminfo.virtual_memory()
                 print(f"Całkowita pamięć: {memoryinfo.total / (1024 ** 3):.2f} GB")
                 print(f"Wolna pamięć: {memoryinfo.available / (1024 ** 3):.2f} GB")
                 print(f"Użycie pamięci: {memoryinfo.percent}%")
 
                 speak(f"Całkowita pamięć RAM to {memoryinfo.total / (1024 ** 3):.2f} Gigabajtów")
-                speak(f"Wolna pamięć RAM to {memoryinfo.available / (1024 ** 3):.2f} Gigabajtów")
+                speak(f"Wolna pamięć RAM to {memoryinfo.available / (1024 ** 3):.2f} Gigabajtów")               #Specyfikacja laptopa
                 speak(f"Użycie pamięci RAM to {memoryinfo.percent}%")
 
             case "statystyki dysku":
+                
                 diskusage = systeminfo.disk_usage('/')
                 print(f"Całkowita pojemność dysku: {diskusage.total / (1024 ** 3):.2f} GB")
                 print(f"Wolne miejsce na dysku: {diskusage.free / (1024 ** 3):.2f} GB")
@@ -114,12 +148,14 @@ while end:
                 speak(f"Całkowita pojemność dysku to {diskusage.total / (1024 ** 3):.2f} Gigabajtów")
                 speak(f"Wolne miejsce na dysku to {diskusage.free / (1024 ** 3):.2f} Gigabajtów")
                 speak(f"Użycie dysku to {diskusage.percent}%")
-
+                
             case "wyłącz się" | "zamknij":
                 speak("Do widzenia")
                 end = False
+                
             case _:
-                speak("Nie zrozumiano komendy")
+                if not searcher(command):
+                    speak("Nie zrozumiano komendy")
+                
     else:
         print("Komenda nie zawierała słowa JARVIS")
-        speak("Komenda nie zawierała słowa dżarwis")
